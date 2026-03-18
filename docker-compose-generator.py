6b97e85b1243dd06aa046cb7907c7b4c4a0bdd6f
bmorselrtto <bmorseletto@fi.uba.ar>
2026-03-18 17:45:42 -0300

    fix:  volume route

--- docker-compose-generator.py
+++ docker-compose-generator.py
@@ -44,7 +44,7 @@ def yaml_generator(client_amount):
                          "networks": [
                              "testing_net"],
                          "volumes": [
-                             "./server/config.ini:/client/config.ini"]
+                             "./server/config.ini:/config.ini"]
                             }
                         },
                  "networks":{
@@ -58,7 +58,7 @@ def yaml_generator(client_amount):
                 }
     for n in range(client_amount):
         client = f"client{n+1}"
-        yaml_dicc["services"][client] = {"container_name": client, "image": "client:latest", "entrypoint": "/client", "environment": [f"CLI_ID={n+1}", "CLI_LOG_LEVEL=DEBUG"], "networks":["testing_net"], "depends_on":["server"], "volumes": ["./client/config.yaml:/client/config.yaml"]}
+        yaml_dicc["services"][client] = {"container_name": client, "image": "client:latest", "entrypoint": "/client", "environment": [f"CLI_ID={n+1}", "CLI_LOG_LEVEL=DEBUG"], "networks":["testing_net"], "depends_on":["server"], "volumes": ["./client/config.yaml:/config.yaml"]}
     return yaml_dicc
 
 if __name__ == "__main__":
